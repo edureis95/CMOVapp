e3b42a09b6886e27fffd302828a5ba2676fb0da6
ricardohtduarte <ricardohtduarte@gmail.com>
2017-10-25 18:37:02 +0100

    Organize main activity

--- app/src/main/java/com/cmov/acme/ui/MainActivity.java
+++ app/src/main/java/com/cmov/acme/ui/MainActivity.java
@@ -1,23 +1,55 @@
 package com.cmov.acme.ui;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.cmov.acme.R;
+import com.google.zxing.integration.android.IntentIntegrator;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button login_button;
+    private Button shop_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        login_button = (Button)findViewById(R.id.login_button);
+        final Activity activity = this;
+        login_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        shop_button = (Button)findViewById(R.id.shop_button);
+        shop_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ShopActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
 
         /*  LOGIN
 
