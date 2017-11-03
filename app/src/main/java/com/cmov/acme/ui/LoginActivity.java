68479b9130d81c46882e3c6420c95aa58abac198
maurr <maurr@users.noreply.github.com>
2017-11-03 17:07:03 +0000

    nfc started

--- app/src/main/java/com/cmov/acme/ui/LoginActivity.java
+++ app/src/main/java/com/cmov/acme/ui/LoginActivity.java
@@ -64,7 +64,8 @@ public class LoginActivity extends AppCompatActivity {
 
     View.OnClickListener registerHandler = new View.OnClickListener() {
         public void onClick(View v) {
-            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            Intent intent = new Intent(LoginActivity.this, NFC_send.class);
+            // Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
         }
     };
---
+++ app/src/main/java/com/cmov/acme/ui/NFC_receive.java
@@ -0,0 +1,54 @@
+package com.cmov.acme.ui;
+
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cmov.acme.R;
+
+public class NFC_receive extends AppCompatActivity {
+
+    private TextView textview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_receive);
+        textview = (TextView) findViewById(R.id.textView7);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        textview.setText(new String(msg.getRecords()[0].getPayload()));
+    }
+}
